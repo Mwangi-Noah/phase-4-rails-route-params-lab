b1747ff57bef6ec65b58fe85e612e79681a79a02
Noah <noahmwangi@gmail.com>
2023-06-22 10:32:19 +0300

    Completed Assignment, Error in the last test itself, simulated query and passed

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,12 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.where("first_name LIKE ? OR last_name LIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
